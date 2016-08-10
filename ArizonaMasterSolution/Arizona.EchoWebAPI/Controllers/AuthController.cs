71e578ca6e779f52d965f0a3d2bc4dd8fcad3556
plsft <george@plsft.com>
2016-08-10 19:01:54 -0400

    latest

--- ArizonaMasterSolution/Arizona.EchoWebAPI/Controllers/AuthController.cs
+++ ArizonaMasterSolution/Arizona.EchoWebAPI/Controllers/AuthController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Web.Http;
@@ -24,7 +21,7 @@ namespace Arizona.EchoWebAPI.Controllers
         [HttpPost]
         public HttpResponseMessage Login(FormDataCollection f)
         {
-            return new HttpResponseMessage() { StatusCode = HttpStatusCode.Accepted };
+            return new HttpResponseMessage() { StatusCode = HttpStatusCode.Accepted  };
         }
 
     }
